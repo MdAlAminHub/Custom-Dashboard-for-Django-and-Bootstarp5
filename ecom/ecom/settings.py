a925892f3e88b54b79ecc00324290181176fcd5a
imimran <engr.aih@gmail.com>
2021-12-14 18:32:37 +0600

    cart and checkout updated

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -154,6 +154,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -36,5 +36,6 @@ urlpatterns = [
    
     
     # path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+]  
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
