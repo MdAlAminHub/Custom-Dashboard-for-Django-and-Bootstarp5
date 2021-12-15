42c44a13440c7aa20c42cc3159adb1e4845ef3f5
imimran <engr.aih@gmail.com>
2021-12-15 18:30:33 +0600

    cart and order method updated

--- ecom/static/js/cart.js
+++ ecom/static/js/cart.js
@@ -1,5 +1,3 @@
-// console.log('hello world')
-
 var updateBtns = document.getElementsByClassName('update-cart')
 
 for (var i = 0; i < updateBtns.length; i++) {
@@ -8,41 +6,37 @@ for (var i = 0; i < updateBtns.length; i++) {
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
 
-//         console.log('USER:', user)
-//         if (user == 'AnonymousUser') {
-//             console.log('Not Logged in')
-//         } else {
-//             updateUserOrder(productId, action)
-
-//             var url = '/update_item/'
-
-//             fetch(url, {
-//                     method: 'POST',
-//                     headers: {
-//                         'Content-Type': 'application/json',
-//                         'X-CSRFToken': csrftoken,
-//                     },
-//                     body: JSON.stringify({
-//                         'productId': productId,
-//                         'action': action
-//                     })
-//                 })
-
-//                 .then((response) => {
-//                     return response.json()
-//                 })
-
-//                 .then((data) => {
-//                     console.log('data:', data)
-//                 })
-//         }
+        console.log('USER:', user)
+        if (user === 'AnonymousUser') {
+            console.log('Not Logged in')
+        } else {
+            updateUserOrder(productId, action)
+        }
 
     })
 
 }
 
-// function updateUserOrder(productId, action) {
-//     console.log('User is logged in, sending data ...')
+function updateUserOrder(productId, action) {
+    console.log('User is logged in, sending data ...')
 
+    var url = '/update_item/'
 
-// }
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId': productId,'action': action})
+    })
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        console.log('data:', data)
+        location.reload()
+    })
+
+}
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,12 +1,25 @@
+import json
 from django.shortcuts import render
+
 from django.http import JsonResponse
 from .models import *
-import json
+
 
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
+        
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
 
 
@@ -16,11 +29,13 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else: 
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items':0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order.get_cart_items
         
-    context = {'items': items, 'order':order}
+    context = {'items': items, 'order':order, 'cartItems':cartItems}
     
     return render(request, 'store/cart.html', context)
 
@@ -32,16 +47,19 @@ def checkout(request):
         order, created = Order.objects.get_or_create(
             customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+        
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order.get_cart_items
 
-    context = {'items': items, 'order': order}
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     
     return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
-    data = json.loads(request.data)
+    data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
     print('Action:', action)
@@ -65,3 +83,8 @@ def updateItem(request):
            
     
     return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+    print('Data:',request.body)
+    return JsonResponse('Payment complete!', safe=False)
+    
